a9921313ca7beb369353e492b08e25c53fb53581
Byron Dover <byrondover+github@gmail.com>
2017-12-28 22:07:46 -0800

    NgModule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AwesomePipe } from './contact/awesome.pipe';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService } from './contact/contact.service';
+import { ContactHighlightDirective } from './contact/contact-highlight.directive';
 import { HighlightDirective } from './highlight.directive';
 import { TitleComponent } from './title.component';
 import { UserService } from './user.service';
@@ -11,13 +16,21 @@ import { UserService } from './user.service';
   declarations: [
     AppComponent,
     HighlightDirective,
-    TitleComponent
+    TitleComponent,
+
+    AwesomePipe,
+    ContactComponent,
+    ContactHighlightDirective
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [UserService],
+  providers: [
+    ContactService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
